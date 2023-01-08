e512f7ec5e80622ffae2390364313b8e0c81b6dd
Siyam <itsyakubsiyam@gmail.com>
2023-01-08 23:41:42 +0600

    Adding controller and routes

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,39 @@
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+};
+
+// @desc    Set goal
+// @route   POST/api/goals
+// @access  Private
+const setGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
+  res.status(200).json({ message: "Set Goal" });
+};
+
+// @desc    Update goals
+// @route   PUT /api/goals/:id
+// @access  Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update Goal ${req.params.id}` });
+};
+
+// @desc    Delete goals
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("hello World!!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/GoalRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
