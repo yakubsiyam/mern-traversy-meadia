d9b6b6d9f9c30c3ba5bab360432220cb232d9abf
yakubsiyam <itsyakubsiyam@gmail.com>
2023-01-11 18:48:06 +0600

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const colors = require('mongoose');
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require('./config/db');
 port = process.env.PORT || 5000;
 
+
+connectDB();
 const app = express();
 
 app.use(express.json());
